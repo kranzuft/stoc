188ae9a9d5f3cf50f0c7329611583ea1c8edb88e
Aden Prior <adenprior@MacBook-Pro.local>
2021-03-03 22:13:52 +1100

    Initial code commit

---
+++ cmd/lofty/search.go
@@ -0,0 +1,49 @@
+package lofty
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func searchPipeMode() {
+	reader := bufio.NewReader(os.Stdin)
+	command := strings.Join(os.Args[1:], " ")
+	cont := true
+	for cont {
+		line, err := reader.ReadString('\n')
+		if err != nil && err == io.EOF {
+			cont = false
+		}
+		if SearchString(command, line) {
+			fmt.Println(strings.Trim(line, "\n"))
+		}
+	}
+}
+
+func splitAndSearch(command string, target string, sep string) bool {
+	for _, line := range strings.Split(target, sep) {
+		SearchString(command, line)
+	}
+	return true
+}
+
+func SearchString(command string, target string) bool {
+	var raw = []rune(command)
+
+	lexSuccess, tokens := parseBooleanAlgebraToTokens(raw)
+
+	if lexSuccess {
+		shuntingSuccess, result := tokenShuntingAlgorithm(tokens)
+
+		if shuntingSuccess {
+			return searchPostfixTokens(result, target)
+		} else {
+			fmt.Println("The calculation was not successful, check that you aren't missing a bracket")
+		}
+	}
+
+	return false
+}
